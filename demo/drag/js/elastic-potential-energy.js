3259d9aac70ee3a4b9ff83b904347e810ae906ee
Linqing Wu <Linqing Wu>
2016-07-02 02:10:09 +0800

    Site updated: 2016-07-02 02:10:07

--- demo/drag/js/elastic-potential-energy.js
+++ demo/drag/js/elastic-potential-energy.js
@@ -30,10 +30,7 @@ function fly() {
         }
         speed *= 0.98;
         var curL = _this.offsetLeft + speed;
-        if (curL <= _this.minL) {
-            curL = _this.minL;
-            speed *= -1;
-        } else if (curL >= _this.maxL) {
+        if (curL <= _this.minl)="" {="" curl="_this.minL;" speed="" *="-1;" }="" else="" if="" (curl="">= _this.maxL) {
             curL = _this.maxL;
             speed *= -1;
         }
@@ -68,3 +65,4 @@ function drop() {
         _this.style.top = curT + "px";
     }, 15);
 }
+</=>
